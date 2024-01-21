4cf967077c9bcb97a5d472c40a6042e0c84d592d
bayuawe <bayuaryandi21@gmail.com>
2024-01-21 15:02:12 +0700

    added city and districts polygon

--- app.py
+++ app.py
@@ -6,23 +6,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def map():
-    # Load GeoJSON data for districts
-    binawidya_geojson = 'data/kecamatan/binawidya.geojson'
-    bukitraya_geojson = 'data/kecamatan/bukitraya.geojson'
-    kulim_geojson = 'data/kecamatan/kulim.geojson'
-    limapuluh_geojson = 'data/kecamatan/limapuluh.geojson'
-    marpoyandamai_geojson = 'data/kecamatan/marpoyandamai.geojson'
-    payungsekaki_geojson = 'data/kecamatan/payungsekaki.geojson'
-    pekanbarukota_geojson = 'data/kecamatan/pekanbarukota.geojson'
-    rumbai_geojson = 'data/kecamatan/rumbai.geojson'
-    rumbaibarat_geojson = 'data/kecamatan/rumbaibarat..geojson'
-    rumbaitimur_geojson = 'data/kecamatan/rumbaitimur.geojson'
-    sail_geojson = 'data/kecamatan/sail.geojson'
-    senapelan_geojson = 'data/kecamatan/senapelan.geojson'
-    sukajadi_geojson = 'data/kecamatan/sukajadi.geojson'
-    tenayanraya_geojson = 'data/kecamatan/tenayanraya.geojson'
-    tuahmadani_geojson = 'data/kecamatan/tuahmadani.geojson'
-
+    # Load GeoJSON data for city
+    kota_pekanbaru_geojson = 'data/pekanbaru_districts/kota_pekanbaru.geojson'
+    
+    # Load GeoJSON data for city
+    bukit_raya_geojson = 'data/pekanbaru_districts/bukit_raya.geojson'
+    limapuluh_geojson = 'data/pekanbaru_districts/limapuluh.geojson'
+    marpoyan_damai_geojson = 'data/pekanbaru_districts/marpoyan_damai.geojson'
+    payung_sekaki_geojson = 'data/pekanbaru_districts/payung_sekaki.geojson'
+    pekanbaru_kota_geojson = 'data/pekanbaru_districts/pekanbaru_kota.geojson'
+    rumbai_pesisir_geojson = 'data/pekanbaru_districts/rumbai_pesisir.geojson'
+    rumbai_geojson = 'data/pekanbaru_districts/rumbai.geojson'
+    sail_geojson = 'data/pekanbaru_districts/sail.geojson'
+    senapelan_geojson = 'data/pekanbaru_districts/senapelan.geojson'
+    sukajadi_geojson = 'data/pekanbaru_districts/sukajadi.geojson'
+    tampan_geojson = 'data/pekanbaru_districts/tampan.geojson'
+    tenayan_raya_geojson = 'data/pekanbaru_districts/tenayan_raya.geojson'
+    
     # Load GeoJSON data for coffee shops from CSV
     coffee_shop_csv = 'data/coffeeshop/coffee_shops.csv'
     coffee_shop_df = pd.read_csv(coffee_shop_csv)
@@ -53,22 +53,22 @@ def map():
     # Create a Folium Map centered at Pekanbaru
     m = folium.Map(location=[0.5394, 101.4431], zoom_start=13)
 
+    # Add GeoJSON layer for city
+    folium.GeoJson(kota_pekanbaru_geojson, name='Kota Pekanbaru').add_to(m)
+    
     # Add GeoJSON layer for districts
-    folium.GeoJson(binawidya_geojson, name='Binawidya').add_to(m)
-    folium.GeoJson(bukitraya_geojson, name='Bukit Raya').add_to(m)
-    folium.GeoJson(kulim_geojson, name='Kulim').add_to(m)
-    folium.GeoJson(limapuluh_geojson, name='Lima Puluh').add_to(m)
-    folium.GeoJson(marpoyandamai_geojson, name='Marpoyan Damai').add_to(m)
-    folium.GeoJson(payungsekaki_geojson, name='Payung Sekaki').add_to(m)
-    folium.GeoJson(pekanbarukota_geojson, name='Pekanbaru Kota').add_to(m)
-    folium.GeoJson(rumbaibarat_geojson, name='Rumbai Barat').add_to(m)
-    folium.GeoJson(rumbai_geojson, name='Rumbai').add_to(m)
-    folium.GeoJson(rumbaitimur_geojson, name='Rumbai Timur').add_to(m)
-    folium.GeoJson(sail_geojson, name='Sail').add_to(m)
-    folium.GeoJson(senapelan_geojson, name='Senapelan').add_to(m)
-    folium.GeoJson(sukajadi_geojson, name='Sukajadi').add_to(m)
-    folium.GeoJson(tuahmadani_geojson, name='Tuah Madani').add_to(m)
-    folium.GeoJson(tenayanraya_geojson, name='Tenayan Raya').add_to(m)
+    folium.GeoJson(bukit_raya_geojson, name='Kecamatan Tampan').add_to(m)
+    folium.GeoJson(limapuluh_geojson, name='Kecamatan Limapuluh').add_to(m)
+    folium.GeoJson(marpoyan_damai_geojson, name='Kecamatan Marpoyan Damai').add_to(m)
+    folium.GeoJson(payung_sekaki_geojson, name='Kecamatan Payung Sekaki').add_to(m)
+    folium.GeoJson(pekanbaru_kota_geojson, name='Kecamatan Pekanbaru Kota').add_to(m)
+    folium.GeoJson(rumbai_pesisir_geojson, name='Kecamatan Rumbai Pesisir').add_to(m)
+    folium.GeoJson(rumbai_geojson, name='Kecamatan Rumbai').add_to(m)
+    folium.GeoJson(sail_geojson, name='Kecamatan Sail').add_to(m)
+    folium.GeoJson(senapelan_geojson, name='Kecamatan Senapelan').add_to(m)
+    folium.GeoJson(sukajadi_geojson, name='Kecamatan Sukajadi').add_to(m)
+    folium.GeoJson(tampan_geojson, name='Kecamatan Tampan').add_to(m)
+    folium.GeoJson(tenayan_raya_geojson, name='Kecamatan Tenayan Raya').add_to(m)
 
     # Add GeoJSON layer for coffee shops
     folium.GeoJson(coffee_shop_geojson, name='Coffee Shops').add_to(m)
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\si-geography\venv
+set VIRTUAL_ENV=D:\gis-flask-pekanbaru\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
